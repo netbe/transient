e5fd5ccb5c6522813ffdf5b7e8b44bfcb1249f78
François Benaiteau <francois.benaiteau@sinnerschrader-mobile.com>
2014-11-25 00:02:05 +0100

    State: add source reference and check for upgrade of app

--- transient/AppDelegate.m
+++ transient/AppDelegate.m
@@ -34,7 +34,35 @@
 
 - (BOOL)application:(UIApplication *)application shouldRestoreApplicationState:(NSCoder *)coder
 {
+    // see https://github.com/kharrison/CodeExamples/blob/master/Restorer/Restorer/UYLAppDelegate.m
+    
     // can check if current bundle version is different from saved applicationState
+    // Retrieve the Bundle Version Key so we can check if the restoration data is from an older
+    // version of the App that would not make sense to restore. This might be the case after we
+    // have made significant changes to the view hierarchy.
+    NSDictionary *infoDictionary = [[NSBundle mainBundle]infoDictionary];
+    NSString *buildVersion = infoDictionary[(NSString*)kCFBundleVersionKey];
+    
+    NSString *restorationBundleVersion = [coder decodeObjectForKey:UIApplicationStateRestorationBundleVersionKey];
+    if ([restorationBundleVersion integerValue] < [buildVersion integerValue])
+    {
+        NSLog(@"Ignoring restoration data for bundle version: %@",restorationBundleVersion);
+        return NO;
+    }
+    
+    // Retrieve the User Interface Idiom (iPhone or iPad) for the device that created the restoration Data.
+    // This allows us to ignore the restoration data when the user interface idiom that created the data
+    // does not match the current device user interface idiom.
+    
+    UIDevice *currentDevice = [UIDevice currentDevice];
+    UIUserInterfaceIdiom restorationInterfaceIdiom = [[coder decodeObjectForKey:UIApplicationStateRestorationUserInterfaceIdiomKey] integerValue];
+    UIUserInterfaceIdiom currentInterfaceIdiom = currentDevice.userInterfaceIdiom;
+    if (restorationInterfaceIdiom != currentInterfaceIdiom)
+    {
+        NSLog(@"Ignoring restoration data for interface idiom: %d",restorationInterfaceIdiom);
+        return NO;
+    }
+    
     return YES;
 }
 

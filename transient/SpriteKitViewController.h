b6a3144a117b41ed4265395d2467e8a770d30dc2
François Benaiteau <francois.benaiteau@sinnerschrader-mobile.com>
2014-11-25 00:02:24 +0100

    SpriteKit: interface

---
+++ transient/SpriteKitViewController.h
@@ -0,0 +1,13 @@
+//
+//  SpriteKitViewController.h
+//  transient
+//
+//  Created by François Benaiteau on 24/11/14.
+//  Copyright (c) 2014 François Benaiteau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SpriteKitViewController : UIViewController
+
+@end
